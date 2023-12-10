0c13c0bfa9af31303e887982ce3f45c9704bb269
YBD21 <santosh.deuja2000@gmail.com>
2023-12-10 20:06:38 +0545

    [server] modify dtos

--- server/src/polls/dtos.ts
+++ server/src/polls/dtos.ts
@@ -1,12 +1,32 @@
 import {
 IsString,
 Length,
-    Min,
-    Max,
+IsInt,
+Min,
+Max
   } from 'class-validator';
 // Dto = Data Transfer Object
 export class CreatePollDto {
 @IsString()
 @Length(1,100)
 topic: string;
+
+@IsInt()
+@Min(1)
+@Max(5)
+votesPerPerson:number;
+
+@IsString()
+@Length(1,25)
+name:string;
+}
+
+export class JoinPollDto {
+ @IsString()
+ @Length(6,6)
+ pollId:string;
+
+ @IsString()
+ @Length(1,25)
+ name:string;
 }
