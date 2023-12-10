28f974a4a36b12376e673d98b30b92074d699ab5
YBD21 <santosh.deuja2000@gmail.com>
2023-12-10 20:14:19 +0545

    [server] modify controllers

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Logger, Post } from "@nestjs/common";
+import { Body, Controller, Logger, Post } from "@nestjs/common";
+import { CreatePollDto, JoinPollDto } from "./dtos";
 
 @Controller('polls')
 export class PollsController {
 
 @Post()
-   async create(){
+   async create(@Body() createPollDto:CreatePollDto){
     Logger.log("In create !");
+    return createPollDto;
    } 
 
 @Post('/join')
-    async join() {
+    async join(@Body() joinPollDto:JoinPollDto) {
         Logger.log("In join");
+        return joinPollDto;
     }
 @Post('/rejoin')
      async rejoin() {
