b5dd591acb201c7a9c0a0e1518244b676e374556
YBD21 <santosh.deuja2000@gmail.com>
2023-12-13 11:24:29 +0545

    [server] updated controlls with dependency injection

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,21 +1,29 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common'
 import { CreatePollDto, JoinPollDto } from './dtos'
+import { PollsServices } from './polls.service'
 
 @Controller('polls')
 export class PollsController {
+  constructor(private pollService: PollsServices) {}
+  // access dependency
   @Post()
   async create(@Body() createPollDto: CreatePollDto) {
-    Logger.log('In create !')
-    return createPollDto
+    const result = await this.pollService.createPoll(createPollDto)
+    return result
   }
 
   @Post('/join')
   async join(@Body() joinPollDto: JoinPollDto) {
-    Logger.log('In join')
-    return joinPollDto
+    const result = await this.pollService.joinPoll(joinPollDto)
+    return result
   }
   @Post('/rejoin')
   async rejoin() {
-    Logger.log('In rejoin !')
+    const result = await this.pollService.reJoinPoll({
+      name: 'from token',
+      pollId: 'Also from token',
+      userId: 'Guess Where This comes From ? Thats Right ! from token',
+    })
+    return result
   }
 }
