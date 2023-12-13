ff7e008a970c5751b5ab0568a04b152c42e2f119
YBD21 <santosh.deuja2000@gmail.com>
2023-12-13 11:17:13 +0545

    [server] updated polls.service with polls.ids

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -4,12 +4,31 @@ import {
   JoinPollFields,
   RejoinPollFields,
 } from './polls.types'
+import { createPollId, createUserId } from './polls.ids'
 
 @Injectable() // provide this service to module
 export class PollsServices {
-  async createPoll(fields: CreatePollFields) {}
+  async createPoll(fields: CreatePollFields) {
+    const pollId = createPollId()
+    const userId = createUserId()
 
-  async joinPoll(fields: JoinPollFields) {}
+    return {
+      ...fields,
+      pollId,
+      userId,
+    }
+  }
 
-  async reJoinPoll(fields: RejoinPollFields) {}
+  async joinPoll(fields: JoinPollFields) {
+    const userId = createUserId()
+
+    return {
+      ...fields,
+      userId,
+    }
+  }
+
+  async reJoinPoll(fields: RejoinPollFields) {
+    return fields
+  }
 }
