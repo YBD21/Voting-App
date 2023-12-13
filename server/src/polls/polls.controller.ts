0bbbac5441d10590a341270084036be9d9b3198d
YBD21 <santosh.deuja2000@gmail.com>
2023-12-13 10:51:53 +0545

    [server] added extra code formater

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,22 +1,21 @@
-import { Body, Controller, Logger, Post } from "@nestjs/common";
-import { CreatePollDto, JoinPollDto } from "./dtos";
+import { Body, Controller, Logger, Post } from '@nestjs/common'
+import { CreatePollDto, JoinPollDto } from './dtos'
 
 @Controller('polls')
 export class PollsController {
+  @Post()
+  async create(@Body() createPollDto: CreatePollDto) {
+    Logger.log('In create !')
+    return createPollDto
+  }
 
-@Post()
-   async create(@Body() createPollDto:CreatePollDto){
-    Logger.log("In create !");
-    return createPollDto;
-   } 
-
-@Post('/join')
-    async join(@Body() joinPollDto:JoinPollDto) {
-        Logger.log("In join");
-        return joinPollDto;
-    }
-@Post('/rejoin')
-     async rejoin() {
-        Logger.log("In rejoin !");
-     }
+  @Post('/join')
+  async join(@Body() joinPollDto: JoinPollDto) {
+    Logger.log('In join')
+    return joinPollDto
+  }
+  @Post('/rejoin')
+  async rejoin() {
+    Logger.log('In rejoin !')
+  }
 }
