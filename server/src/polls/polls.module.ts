5049fc9dada9ceb32e3dce2810e957d36660b2db
YBD21 <santosh.deuja2000@gmail.com>
2024-03-28 17:11:48 +0545

    added polls.repo

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config'
 import { PollsController } from './polls.controller'
 import { PollsServices } from './polls.service'
 import { redisModule } from 'src/modules.config'
+import { PollsRepository } from './polls.repository'
 
 @Module({
   imports: [ConfigModule, redisModule],
   controllers: [PollsController],
-  providers: [PollsServices],
+  providers: [PollsServices, PollsRepository],
 })
 export class PollsModule {}
